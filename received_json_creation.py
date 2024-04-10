6fbde2445bc9bde93c7055d866f655233a17bade
Sahil-kg <155853197+Sahil-kg@users.noreply.github.com>
2024-04-10 22:59:08 +0530

    Add files via upload

--- received_json_creation.py
+++ received_json_creation.py
@@ -1,36 +1,22 @@
 import json
-import random
-import string
 
-def generate_random_color_name():
-    shape_colors = ['white', 'black', 'red', 'blue', 'green', 'purple', 'brown', 'orange']
-    return random.choice(shape_colors)
+shape_colors = ['white', 'black', 'red', 'blue', 'green', 'purple', 'brown', 'orange']
 
-def generate_random_shape():
-    shapes = ['circle', 'semicircle', 'quarter circle', 'triangle','rectangle', 'pentagon', 'star', 'cross']
-    return random.choice(shapes)
+letters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
 
-def generate_random_letter_color():
-    color_names = ['white', 'black', 'red', 'blue', 'green', 'purple', 'brown', 'orange']
-    letter_color = random.choice(color_names)
-    # Ensure letter color is different from color
-    while True:
-        new_letter_color = random.choice(color_names)
-        if new_letter_color != letter_color:
-            return new_letter_color
+shapes = ['circle', 'semicircle', 'quarter circle', 'triangle','rectangle', 'pentagon', 'star', 'cross']
 
-def generate_random_json(num_entries=5):
+color_names = ['white', 'black', 'red', 'blue', 'green', 'purple', 'brown', 'orange']
+
+def generate_random_json(num_entries):
     data = {}
-    category_name = 'to find'  # Change category name to 'to find'
-    color = generate_random_color_name()
-    letter_color = generate_random_letter_color()
-    while color == letter_color:
-        letter_color = generate_random_letter_color()
+    category_name = 'to find'  
+
     value = {
-        'color': color,
-        'shape': generate_random_shape(),
-        'letter_color': letter_color,
-        'letter': random.choice(string.ascii_uppercase)
+        'color': color_names,
+        'shape': shapes,
+        'letter_color': color_names,
+        'letter':letters
     }
     data[category_name] = value
     return data
@@ -40,15 +26,10 @@ def save_json(data, file_path):
         json.dump(data, file, indent=4)
 
 def main():
-    num_entries = 1  # Change number of categories to 1
+    num_entries = 1  
     file_path = '/home/sahil/Documents/test.py/received.json'
-
-    # Generate random JSON data
     json_data = generate_random_json(num_entries)
-
-    # Save JSON data to a file
     save_json(json_data, file_path)
-
     print(f"Random JSON data has been generated and saved to '{file_path}'.")
 
 if __name__ == "__main__":
